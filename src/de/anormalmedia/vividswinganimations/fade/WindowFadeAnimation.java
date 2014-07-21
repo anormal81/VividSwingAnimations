67a973942a4d59e955c54480ba4b158838ddd869
André Seebode <anormal81@gmail.com>
2014-07-21 10:23:48 +0200

    Window fade animation now supports java 6 and 7 api via reflection.

--- src/de/anormalmedia/vividswinganimations/fade/WindowFadeAnimation.java
+++ src/de/anormalmedia/vividswinganimations/fade/WindowFadeAnimation.java
@@ -1,6 +1,7 @@
 package de.anormalmedia.vividswinganimations.fade;
 
 import java.awt.Window;
+import java.lang.reflect.Method;
 
 import de.anormalmedia.vividswinganimations.AbstractAnimation;
 
@@ -10,15 +11,46 @@ public class WindowFadeAnimation extends AbstractAnimation {
     private final float targetAlpha;
 
     private float       initialAlpha;
+    private Method      getOpacity;
+    private Method      setOpacity;
+    private Method      setWindowOpaque;
+    private Method      getWindowOpacity;
+    private Method      setWindowOpacity;
 
     WindowFadeAnimation( Window target, float targetAlpha ) {
         this.target = target;
         this.targetAlpha = targetAlpha;
+
+        try {
+            getOpacity = target.getClass().getMethod( "getOpacity" );
+            setOpacity = target.getClass().getMethod( "setOpacity", float.class );
+        } catch( Throwable t ) {
+            try {
+                Class<?> aWTUtilities = Class.forName( "com.sun.awt.AWTUtilities" );
+                setWindowOpaque = aWTUtilities.getMethod( "setWindowOpaque", Window.class, boolean.class );
+                getWindowOpacity = aWTUtilities.getMethod( "getWindowOpacity", Window.class );
+                setWindowOpacity = aWTUtilities.getMethod( "setWindowOpacity", Window.class, float.class );
+            } catch( Throwable t2 ) {
+                t2.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void prepare() {
-        initialAlpha = target.getOpacity();
+        try {
+            if( setWindowOpaque != null ) {
+                setWindowOpaque.invoke( null, target, Boolean.TRUE );
+            }
+            if( getOpacity != null ) {
+                initialAlpha = ((Float)getOpacity.invoke( target )).floatValue();
+            }
+            if( getWindowOpacity != null ) {
+                initialAlpha = ((Float)getWindowOpacity.invoke( null, target )).floatValue();
+            }
+        } catch( Throwable t ) {
+            t.printStackTrace();
+        }
         super.prepare();
     }
 
@@ -26,6 +58,15 @@ public class WindowFadeAnimation extends AbstractAnimation {
     public void animate( long timeProgress ) {
         float deltaAlpha = targetAlpha - initialAlpha;
         float nextAlpha = Math.max( 0f, Math.min( 1f, initialAlpha + ((float)deltaAlpha / (float)getDuration() * (float)timeProgress) ) );
-        target.setOpacity( nextAlpha );
+        try {
+            if( setOpacity != null ) {
+                setOpacity.invoke( target, Float.valueOf( nextAlpha ) );
+            }
+            if( setWindowOpacity != null ) {
+                setWindowOpacity.invoke( null, target, Float.valueOf( nextAlpha ) );
+            }
+        } catch( Throwable t ) {
+            t.printStackTrace();
+        }
     }
 }
--- src/de/anormalmedia/vividswinganimations/fade/WindowFadeFactory.java
+++ src/de/anormalmedia/vividswinganimations/fade/WindowFadeFactory.java
@@ -1,9 +1,10 @@
 package de.anormalmedia.vividswinganimations.fade;
 
 import java.awt.Container;
+import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
-import java.awt.GraphicsDevice.WindowTranslucency;
 import java.awt.Window;
+import java.lang.reflect.Method;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -18,10 +19,45 @@ public class WindowFadeFactory {
 
     public static Animation createWindowFadeAnimation( Window target, float targetAlpha ) {
         try {
-            GraphicsDevice gd = target.getGraphicsConfiguration().getDevice();
-            if( gd.isWindowTranslucencySupported( WindowTranslucency.TRANSLUCENT ) ) {
-                target.setOpacity( target.getOpacity() );
-                return new WindowFadeAnimation( target, targetAlpha );
+            // Java 7 API
+            try {
+                Class<?> windowTranslucency = Class.forName( "java.awt.GraphicsDevice$WindowTranslucency" );
+                GraphicsDevice gd = target.getGraphicsConfiguration().getDevice();
+                Method isWindowTranslucencySupported = gd.getClass().getMethod( "isWindowTranslucencySupported", windowTranslucency );
+                Object isSupported = isWindowTranslucencySupported.invoke( gd, windowTranslucency.getField( "TRANSLUCENT" ).get( null ) );
+                if( (isSupported instanceof Boolean) && ((Boolean)isSupported).booleanValue() ) {
+                    Method getOpacity = Window.class.getMethod( "getOpacity" );
+                    Method setOpacity = Window.class.getMethod( "setOpacity", float.class );
+                    Float opacity = (Float)getOpacity.invoke( target );
+                    setOpacity.invoke( target, opacity );
+                    return new WindowFadeAnimation( target, targetAlpha );
+                }
+            } catch( ClassNotFoundException t ) {
+                // Java 7 API not supported
+            }
+
+            // Java 6 API
+            try {
+                Class<?> aWTUtilities = Class.forName( "com.sun.awt.AWTUtilities" );
+                Class<?> translucency = Class.forName( "com.sun.awt.AWTUtilities$Translucency" );
+                Method isTranslucencySupported = aWTUtilities.getMethod( "isTranslucencySupported", translucency );
+                Method isTranslucencyCapable = aWTUtilities.getMethod( "isTranslucencyCapable", GraphicsConfiguration.class );
+
+                Object translucencySupported = isTranslucencySupported.invoke( null, translucency.getField( "TRANSLUCENT" ).get( null ) );
+                if( (translucencySupported instanceof Boolean) && ((Boolean)translucencySupported).booleanValue() ) {
+                    Object translucencyCapable = isTranslucencyCapable.invoke( null, target.getGraphicsConfiguration() );
+                    if( (translucencyCapable instanceof Boolean) && ((Boolean)translucencyCapable).booleanValue() ) {
+                        Method setWindowOpaque = aWTUtilities.getMethod( "setWindowOpaque", Window.class, boolean.class );
+                        setWindowOpaque.invoke( null, target, Boolean.TRUE );
+                        Method getWindowOpacity = aWTUtilities.getMethod( "getWindowOpacity", Window.class );
+                        Method setWindowOpacity = aWTUtilities.getMethod( "setWindowOpacity", Window.class, float.class );
+                        Float opacity = (Float)getWindowOpacity.invoke( null, target );
+                        setWindowOpacity.invoke( null, target, opacity );
+                        return new WindowFadeAnimation( target, targetAlpha );
+                    }
+                }
+            } catch( ClassNotFoundException t ) {
+                // Java 6 API not supported
             }
         } catch( Throwable t ) {
             t.printStackTrace();
